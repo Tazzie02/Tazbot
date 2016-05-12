58bea3eb2242639f373d0198ceb654f1a163a7e2
Tazzie02 <Tazzie02@users.noreply.github.com>
2016-05-13 02:21:10 +0930

    Changed OpenWeatherMap class to be super class of the new OWMWeather class. Updated WeatherCommand accordingly.

--- src/main/java/com/tazzie02/tazbot/commands/search/WeatherCommand.java
+++ src/main/java/com/tazzie02/tazbot/commands/search/WeatherCommand.java
@@ -2,18 +2,17 @@ package com.tazzie02.tazbot.commands.search;
 
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
-import java.util.TimeZone;
 
 import org.apache.commons.lang3.StringUtils;
 
 import com.tazzie02.tazbot.commands.Command;
 import com.tazzie02.tazbot.exceptions.QuotaExceededException;
+import com.tazzie02.tazbot.helpers.OWMWeather;
 import com.tazzie02.tazbot.helpers.OpenWeatherMap;
 import com.tazzie02.tazbot.util.SendMessage;
 import com.tazzie02.tazbot.util.TazzieTime;
+import com.tazzie02.tazbot.util.TimeUtil;
 
 import net.dv8tion.jda.events.message.MessageReceivedEvent;
 
@@ -23,9 +22,9 @@ public class WeatherCommand extends Command {
 	public void onCommand(MessageReceivedEvent e, String[] args) {
 		String s = StringUtils.join(args, " ", 1, args.length);
 		try {
-			OpenWeatherMap owm = new OpenWeatherMap(s);
+			OWMWeather weather = new OWMWeather(s);
 			
-			SendMessage.sendMessage(e, weatherInfo(owm));
+			SendMessage.sendMessage(e, weatherInfo(weather));
 		} catch (IOException ex) {
 			SendMessage.sendMessage(e, "Error: Could not connect to web page.");
 		} catch (QuotaExceededException ex) {
@@ -35,60 +34,30 @@ public class WeatherCommand extends Command {
 		}
 	}
 	
-	private String weatherInfo(OpenWeatherMap owm) {
-		if (owm == null) {
+	private String weatherInfo(OWMWeather weather) {
+		if (weather == null) {
 			return "Error: Could not get weather information.";
 		}
 		
-		double temp = owm.getTemp();
+		double temp = weather.getTemp();
 		
-		StringBuilder sb = new StringBuilder();
-		sb.append("Current weather information for ").append(owm.getLocation()).append(", ").append(owm.getCountry()).append(":\n")
-		.append(OpenWeatherMap.kelvinToCelsius(temp)).append("°C, ").append(OpenWeatherMap.kelvinToFahrenheit(temp)).append("°F\n")
-		.append(owm.getWeatherMain(0)).append(", ").append(owm.getWeatherDescription(0)).append("\n")
-		.append("Humidity: ").append(owm.getHumidity()).append("%\n")
-		.append("Wind: ").append(owm.getWindDirectionCardinal()).append(" ").append(owm.getWindSpeed()).append(" meter/s\n")
-		.append("Cloudiness: ").append(owm.getClouds()).append("%\n");
+		String info = "Current weather information for " + weather.getLocation() + ", " + weather.getCountry() + ":\n"
+				+ OpenWeatherMap.kelvinToCelsius(temp) + "°C, " + OpenWeatherMap.kelvinToFahrenheit(temp) + "°F\n"
+				+ weather.getWeatherMain(0) + ", " + weather.getWeatherDescription(0) + "\n"
+				+ "Humidity: " + weather.getHumidity() + "%\n"
+				+ "Wind: " + weather.getWindDirectionCardinal() + " " + weather.getWindSpeed() + " meter/s\n"
+				+ "Cloudiness: " + weather.getClouds() + "%\n";
 		
-		if (owm.getRain() != 0) {
-			sb.append("Rain: ").append(owm.getRain()).append("\n");
+		if (weather.getRain() != 0) {
+			info += "Rain: " + weather.getRain() + "mm\n";
 		}
-		if (owm.getSnow() != 0) {
-			sb.append("Snow: ").append(owm.getSnow()).append("\n");
+		if (weather.getSnow() != 0) {
+			info += "Snow: " + weather.getSnow() + "mm\n";
 		}
-		sb.append("Last updated ").append(timeDifference(owm.getTime())).append(" ago");
-		//sb.append("Last updated ").append(unixTimeToString(owm.getTime()));
+		info += "Last updated " + new TazzieTime(TimeUtil.unixTimeOffset(weather.getTime())).toStringIgnoreZero() + " ago";
 		
-		return sb.toString();
+		return info;
 	}
-	
-	private String timeDifference(long unix) {
-		Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
-		cal.setTime(new Date(unix * 1000));
-		
-		long diff = new Date().getTime() - cal.getTimeInMillis();
-		
-		return new TazzieTime(diff).toStringIgnoreZero();
-	}
-	
-//	private String unixTimeToString(long unix) {
-//		Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
-//		cal.setTime(new Date(unix * 1000));
-//		StringBuilder sb = new StringBuilder()
-//				.append(new DateFormatSymbols().getMonths()[cal.get(Calendar.MONTH)] + " ")
-//				.append(cal.get(Calendar.DAY_OF_MONTH) + " ")
-//				.append(String.format("%02d:%02d ", cal.get(Calendar.HOUR), cal.get(Calendar.MINUTE)));
-//		
-//		if (cal.get(Calendar.AM_PM) == Calendar.AM) {
-//			sb.append("AM");
-//		}
-//		else {
-//			sb.append("PM");
-//		}
-//		sb.append(" UTC");
-//		
-//		return sb.toString();
-//	}
 	
 	@Override
 	public List<String> getAliases() {
--- src/main/java/com/tazzie02/tazbot/helpers/OpenWeatherMap.java
+++ src/main/java/com/tazzie02/tazbot/helpers/OpenWeatherMap.java
@@ -8,27 +8,57 @@ import java.util.Locale;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.math.NumberUtils;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import com.tazzie02.tazbot.exceptions.QuotaExceededException;
 import com.tazzie02.tazbot.managers.ConfigManager;
 import com.tazzie02.tazbot.util.WebUtil;
 
-public class OpenWeatherMap {
+public abstract class OpenWeatherMap {
 	
-	private static final String baseURL = "http://api.openweathermap.org/data/2.5/weather?";
-//	private static final String baseURL = "http://api.openweathermap.org/data/2.5/forecast?"; // TODO Implement 5 day 3 hour forecast
 	private static final String key;
-	private JSONObject obj;
+	protected JSONObject data;
+	
+	protected abstract String getBaseURL();
 	
 	static {
 		key = ConfigManager.getInstance().getConfig().getOpenWeatherMapKey();
 	}
 	
-	public OpenWeatherMap(String query) throws IOException, QuotaExceededException, NullPointerException {
+	public OpenWeatherMap(String query) throws IOException, QuotaExceededException {
 		query = query.replace(",", " ");
 		String[] args = query.split(" ");
+		String search = getSearchURL(args);
+		
+		if (search != null) {
+			search = search + "&appid=" + key;
+			search(search);
+		}
+		else {
+			throw new NullPointerException("Search could not be created.");
+		}
+	}
+	
+	// kelvin - 273.15
+	public static double kelvinToCelsius(double kelvin) {
+		BigDecimal zeroKelvin = BigDecimal.valueOf(-273.15d);
+		BigDecimal kelvinBig = BigDecimal.valueOf(kelvin);
+		return kelvinBig.add(zeroKelvin).doubleValue();
+	}
+	
+	// kelvin * 9 / 5 - 459.67
+	public static double kelvinToFahrenheit(double kelvin) {
+		BigDecimal zeroKelvin = BigDecimal.valueOf(-459.67d);
+		BigDecimal kelvinBig = BigDecimal.valueOf(kelvin);
+		return kelvinBig.multiply(BigDecimal.valueOf(9)).divide(BigDecimal.valueOf(5)).add(zeroKelvin).doubleValue();
+	}
+	
+	public static String getCardinalDirection(double direction) {
+		String dirs[] = {"N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE","S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW", "N"};
+		return dirs[(int) Math.round(direction / 11.25 / 2)];
+	}
+	
+	protected String getSearchURL(String[] args) {
 		String search = null;
 		
 		if (args.length == 1) {
@@ -70,153 +100,11 @@ public class OpenWeatherMap {
 			search = searchByCity(args);
 		}
 		
-		if (search != null) {
-			search = search + "&appid=" + key;
-//			System.out.println(search);
-			search(search);
-		}
-		else {
-			throw new NullPointerException("Search could not be created.");
-		}
+		return search;
 	}
 	
-	public double getLatitude() {
-		return obj.getJSONObject("coord").getDouble("lat");
-	}
-	
-	public double getLongitude() {
-		return obj.getJSONObject("coord").getDouble("lon");
-	}
-	
-	public int getWeatherCount() {
-		return obj.getJSONArray("weather").length();
-	}
-	
-	public int getWeatherId(int index) {
-		return obj.getJSONArray("weather").getJSONObject(index).getInt("id");
-	}
-	
-	public String getWeatherMain(int index) {
-		return obj.getJSONArray("weather").getJSONObject(index).getString("main");
-	}
-	
-	public String getWeatherDescription(int index) {
-		return obj.getJSONArray("weather").getJSONObject(index).getString("description");
-	}
-	
-	public String getWeatherIcon(int index) {
-		return obj.getJSONArray("weather").getJSONObject(index).getString("icon");
-	}
-	
-	public double getTemp() {
-		return obj.getJSONObject("main").getDouble("temp");
-	}
-	
-	public int getPressure() {
-		return obj.getJSONObject("main").getInt("pressure");
-	}
-	
-	public int getHumidity() {
-		return obj.getJSONObject("main").getInt("humidity");
-	}
-	
-	public double getTempMin() {
-		return obj.getJSONObject("main").getDouble("temp_min");
-	}
-	
-	public double getTempMax() {
-		return obj.getJSONObject("main").getDouble("temp_max");
-	}
-	
-	public double getWindSpeed() {
-		return obj.getJSONObject("wind").getDouble("speed");
-	}
-	
-	public int getWindDirectionDegree() {
-		return obj.getJSONObject("wind").getInt("deg");
-	}
-	
-	public String getWindDirectionCardinal() {
-		return getCardinalDirection(getWindDirectionDegree());
-	}
-	
-	public int getClouds() {
-		return obj.getJSONObject("clouds").getInt("all");
-	}
-	
-	public int getRain() {
-		try {
-			return obj.getJSONObject("rain").getInt("3h");
-		}
-		catch (JSONException e) {
-			return 0;
-		}
-	}
-	
-	public int getSnow() {
-		try {
-			return obj.getJSONObject("snow").getInt("3h");
-		}
-		catch (JSONException e) {
-			return 0;
-		}
-	}
-	
-	public long getTime() {
-		return obj.getLong("dt");
-	}
-	
-	public String getCountry() {
-		Locale locale = new Locale("", getCountryCode());
-		return locale.getDisplayCountry();
-	}
-	
-	public String getCountryCode() {
-		return obj.getJSONObject("sys").getString("country");
-	}
-	
-	public long getSunrise() {
-		return obj.getJSONObject("sys").getLong("sunrise");
-	}
-	
-	public long getSunset() {
-		return obj.getJSONObject("sys").getLong("sunset");
-	}
-	
-	public int getId() {
-		return obj.getInt("id");
-	}
-	
-	public String getLocation() {
-		return obj.getString("name");
-	}
-	
-	// kelvin - 273.15
-	public static double kelvinToCelsius(double kelvin) {
-		BigDecimal zeroKelvin = BigDecimal.valueOf(-273.15d);
-		BigDecimal kelvinBig = BigDecimal.valueOf(kelvin);
-		return kelvinBig.add(zeroKelvin).doubleValue();
-	}
-	
-	// kelvin * 9 / 5 - 459.67
-	public static double kelvinToFahrenheit(double kelvin) {
-		BigDecimal zeroKelvin = BigDecimal.valueOf(-459.67d);
-		BigDecimal kelvinBig = BigDecimal.valueOf(kelvin);
-		return kelvinBig.multiply(BigDecimal.valueOf(9)).divide(BigDecimal.valueOf(5)).add(zeroKelvin).doubleValue();
-	}
-	
-	public static String getCardinalDirection(double direction) {
-		String dirs[] = {"N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE","S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW", "N"};
-		return dirs[(int) Math.round(direction / 11.25 / 2)];
-	}
-	
-	private void search(String url) throws IOException, QuotaExceededException {
-		String json = WebUtil.getWebPage(url);
-		obj = new JSONObject(json);
-	}
-	
-	private String searchByZip(String zip, String countryCode) {
-		String search = baseURL + "zip=";
+	protected String searchByZip(String zip, String countryCode) {
+		String search = getBaseURL() + "zip=";
 		if (countryCode == null) {
 			search = search + zip;
 		}
@@ -226,13 +114,13 @@ public class OpenWeatherMap {
 		return search;
 	}
 	
-	private String searchByCoords(String latitude, String longitude) {
-		String search = baseURL + "lat=" + latitude + "&lon=" + longitude;
+	protected String searchByCoords(String latitude, String longitude) {
+		String search = getBaseURL() + "lat=" + latitude + "&lon=" + longitude;
 		return search;
 	}
 	
-	private String searchByCity(String city, String countryCode) {
-		String search = baseURL + "q=";
+	protected String searchByCity(String city, String countryCode) {
+		String search = getBaseURL() + "q=";
 		if (countryCode == null) {
 			search = search + city;
 		}
@@ -242,7 +130,7 @@ public class OpenWeatherMap {
 		return search;
 	}
 	
-	private String searchByCity(String[] args) {
+	protected String searchByCity(String[] args) {
 		try {
 			if (checkCountryCode(args[args.length - 1])) {
 				return searchByCity(URLEncoder.encode(StringUtils.join(args, " ", 0, args.length - 1), "UTF-8"), args[args.length - 1]);
@@ -255,7 +143,7 @@ public class OpenWeatherMap {
 		return null;
 	}
 	
-	private boolean checkCountryCode(String countryCode) {
+	protected boolean checkCountryCode(String countryCode) {
 		// All country codes must be 2 characters
 		if (countryCode.length() != 2) {
 			return false;
@@ -267,6 +155,11 @@ public class OpenWeatherMap {
 		}
 		
 		return true;
+	}
+	
+	protected void search(String url) throws IOException, QuotaExceededException {
+		String json = WebUtil.getWebPage(url);
+		data = new JSONObject(json);
 	}
 	
 }
