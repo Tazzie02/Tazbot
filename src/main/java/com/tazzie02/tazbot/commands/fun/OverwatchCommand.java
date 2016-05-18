72324aad72032ab76872758a7c50496dadb67fb9
Tazzie02 <Tazzie02@users.noreply.github.com>
2016-05-19 04:56:00 +0930

    Updated Overwatch release time.

--- src/main/java/com/tazzie02/tazbot/commands/fun/OverwatchCommand.java
+++ src/main/java/com/tazzie02/tazbot/commands/fun/OverwatchCommand.java
@@ -17,19 +17,12 @@ public class OverwatchCommand extends Command {
 	public void onCommand(MessageReceivedEvent e, String[] args) {
 		Calendar current = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
 		Calendar release = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
-		Calendar ends = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
 		
-		release.set(2016, 4, 25, 0, 0, 0);
-		ends.set(2016, 4, 10, 17, 0, 0);
+		release.set(2016, 4, 23, 23, 0, 0);
 		
 		long releaseMs = release.getTimeInMillis() - current.getTimeInMillis();
-		long endsMs = ends.getTimeInMillis() - current.getTimeInMillis();
 		
 		String s = "";
-		if (endsMs >= 0) {
-			TazzieTime time = new TazzieTime(endsMs);
-			s += "BETA ENDS IN        `" + timeToString(time) + "`\n";
-		}
 		if (releaseMs >= 0) {
 			TazzieTime time = new TazzieTime(releaseMs);
 			s += "RETAIL RELEASE  `" + timeToString(time) + "`";
