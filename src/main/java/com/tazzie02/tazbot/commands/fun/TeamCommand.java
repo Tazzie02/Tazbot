b650da0b181de35e082dee516631c934c08973e8
Tazzie02 <Tazzie02@users.noreply.github.com>
2016-06-25 00:22:59 +0930

    Added additional functionality to team command.

--- src/main/java/com/tazzie02/tazbot/commands/fun/TeamCommand.java
+++ src/main/java/com/tazzie02/tazbot/commands/fun/TeamCommand.java
@@ -5,48 +5,85 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
+
 import com.tazzie02.tazbot.commands.Command;
+import com.tazzie02.tazbot.util.JDAUtil;
 import com.tazzie02.tazbot.util.SendMessage;
 
 import net.dv8tion.jda.entities.User;
+import net.dv8tion.jda.entities.VoiceChannel;
 import net.dv8tion.jda.events.message.MessageReceivedEvent;
 
 public class TeamCommand implements Command {
 	
-	// TODO Greater configuration options
-
 	@Override
 	public void onCommand(MessageReceivedEvent e, String[] args) {
 		List<User> users = new ArrayList<User>();
-		users.addAll(e.getMessage().getMentionedUsers());
-		int size = users.size();
 		
-		if (users.isEmpty()) {
-			SendMessage.sendMessage(e, "Error: No users mentioned.");
+		users.addAll(e.getMessage().getMentionedUsers());
+		
+		if (args.length == 1) {
+			SendMessage.sendMessage(e, "Error: No users were added.");
+			return;
 		}
-		else {
-			List<User> team1 = new ArrayList<User>();
-			List<User> team2 = new ArrayList<User>();
-			
-			Collections.shuffle(users);
-			
-			for (int i = 0; i < size; i++) {
-				if (i % 2 == 0) {
-					team1.add(users.get(i));
-				}
-				else {
-					team2.add(users.get(i));
+		
+		int teamCount = 2;
+		int i = 1;
+		if (args.length > 2 && NumberUtils.isDigits(args[1])) {
+			teamCount = Integer.parseInt(args[1]);
+			i = 2;
+		}
+		
+		while (i < args.length) {
+			// Channel position
+			if (NumberUtils.isDigits(args[i])) {
+				Integer position = Integer.parseInt(args[i]);
+				VoiceChannel vc = JDAUtil.getVoiceChannelAtPosition(e.getGuild(), position);
+				if (vc != null) {
+					users.addAll(vc.getUsers());
 				}
 			}
-			
-			StringBuilder sb = new StringBuilder();
-			sb.append("Team 1: ");
-			team1.stream().forEach(u -> sb.append(u.getUsername() + " "));
-			sb.append("\nTeam 2: ");
-			team2.stream().forEach(u -> sb.append(u.getUsername() + " "));
-			
-			SendMessage.sendMessage(e, sb.toString());
+			i++;
 		}
+		
+		if (users.isEmpty()) {
+			SendMessage.sendMessage(e, "Error: No users were added.");
+			return;
+		}
+		
+		List<List<User>> teams = shuffleTeams(users, teamCount);
+		
+		StringBuilder output = new StringBuilder();
+		for (int j = 0; j < teams.size(); j++) {
+			List<User> team = teams.get(j);
+			List<String> usernames = new ArrayList<String>();
+			for (int k = 0; k < team.size(); k++) {
+				User u = team.get(k);
+				usernames.add(u.getUsername());
+			}
+			
+			output.append(String.format("Team %d: %s\n", (j + 1), StringUtils.join(usernames, ", ")));
+		}
+		
+		SendMessage.sendMessage(e, output.toString());
+	}
+	
+	private List<List<User>> shuffleTeams(List<User> users, int teamCount) {
+		List<List<User>> teams = new ArrayList<List<User>>();
+		
+		for (int i = 0; i < teamCount; i++) {
+			teams.add(new ArrayList<User>());
+		}
+		
+		Collections.shuffle(users);
+		
+		for (int i = 0; i < users.size(); i++) {
+			teams.get(i % teamCount).add(users.get(i));
+		}
+		
+		return teams;
 	}
 	
 	@Override
@@ -71,7 +108,11 @@ public class TeamCommand implements Command {
 
 	@Override
 	public String getUsageInstructions() {
-		return "team <@user> <...> - Randomly split users only two teams.";
+		return "team <@user> <...> - Randomly split users into two teams.\n"
+				+ "team <teamCount> <@user> <...> - Randomly split users in to teamCount teams."
+				+ "team <voicePosition> - Randomly split users at position voicePosition into two teams.\n"
+				+ "team <teamCount> <voicePosition> <...> - Randomly split users into teamCount teams."
+				+ "**NOTE:** If multiple voicePositions are mentioned, the first argument MUST be teamCount.";
 	}
 
 	@Override
