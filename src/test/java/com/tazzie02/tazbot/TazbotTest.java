99ff87d145e211acf1950f03edc2ac0c1433769f
Tazzie02 <Tazzie02@users.noreply.github.com>
2016-08-23 21:07:50 +0930

    Added testTest.

---
+++ src/test/java/com/tazzie02/tazbot/TazbotTest.java
@@ -0,0 +1,14 @@
+package com.tazzie02.tazbot;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TazbotTest {
+	
+	@Test
+	public void testTest() {
+		assertTrue(true);
+	}
+
+}
