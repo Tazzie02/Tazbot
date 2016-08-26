c3c5e78f902b141202e998436fd669fcf4f56cc9
Tazzie02 <Tazzie02@users.noreply.github.com>
2016-08-26 16:28:42 +0930

    Added shakespearean insults.

--- src/main/java/com/tazzie02/tazbot/commands/fun/InsultCommand.java
+++ src/main/java/com/tazzie02/tazbot/commands/fun/InsultCommand.java
@@ -13,19 +13,43 @@ import com.tazzie02.tazbot.util.SendMessage;
 import net.dv8tion.jda.events.message.MessageReceivedEvent;
 
 public class InsultCommand implements Command {
+	
+	private final String INSULT_GENERAL = "http://www.insultgenerator.org";
+	private final String INSULT_SHAKESPEARE = "http://www.pangloss.com/seidel/Shaker/";
+	
+	private final String ERROR = "Error: Cannot get an insult at this time.";
 
 	@Override
 	public void onCommand(MessageReceivedEvent e, String[] args) {
-		String url = "http://www.insultgenerator.org";
-		
 		try {
-			Document doc = Jsoup.connect(url).get();
-			String insult = doc.select(".wrap").text();
+			String insult = null;
 			
-			SendMessage.sendMessage(e, insult);
-		} catch (IOException e1) {
-			SendMessage.sendMessage(e, "Error: Cannot get an insult at this time.");
+			if (args.length > 1 && args[1].equalsIgnoreCase("shakespeare") || args[1].equalsIgnoreCase("shakespearean")) { 
+				insult = getInsultShakespeare();
+			}
+			else {
+				insult = getInsultGeneral();
+			}
+			
+			if (insult != null) {
+				SendMessage.sendMessage(e, insult);
+			}
+			else {
+				SendMessage.sendMessage(e, ERROR);
+			}
+		} catch (IOException ex) {
+			SendMessage.sendMessage(e, ERROR);
 		}
+	}
+	
+	private String getInsultGeneral() throws IOException {
+		Document doc = Jsoup.connect(INSULT_GENERAL).get();
+		return doc.select(".wrap").text();
+	}
+	
+	private String getInsultShakespeare() throws IOException {
+		Document doc = Jsoup.connect(INSULT_SHAKESPEARE).get();
+		return doc.select("font").text();
 	}
 	
 	@Override
@@ -40,17 +64,18 @@ public class InsultCommand implements Command {
 
 	@Override
 	public String getDescription() {
-		return null;
+		return "Print an insult.";
 	}
 
 	@Override
 	public String getName() {
-		return null;
+		return "Insult Command";
 	}
 
 	@Override
 	public String getUsageInstructions() {
-		return null;
+		return "insult - Print a random insult from <" + INSULT_GENERAL + ">\n"
+				+ "insult shakespeare - Print a random insult from <" + INSULT_SHAKESPEARE + ">";
 	}
 
 	@Override
